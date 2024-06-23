f700514a51e3d2750a082d8a8f1a3fd687868420
omerkarakas <karakasomer@gmail.com>
2024-06-23 12:52:17 +0300

    nextui integration

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,15 @@
-import Image from "next/image";
+import { Input } from "@nextui-org/react";
 
 export default function Home() {
-  return <main className="p-12">Hi Next.js project!</main>;
+  return (
+    <main className="p-12">
+      Hi Next.js project!
+      <div className="flex flex-col gap-4 w-1/4">
+        <label htmlFor="name">Name</label>
+        <input id="name" />
+
+        <Input type="email" label="Email" />
+      </div>
+    </main>
+  );
 }
